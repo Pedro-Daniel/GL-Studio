180919daae51ce9149063c4a57192b9e89932f66
Pedro-Daniel <pedrobomtempo2001@gmail.com>
2021-04-07 19:25:34 -0300

    Correção de erros

--- GL Studio/Main/Mute.gd
+++
@@ -1,9 +0,0 @@
-extends CheckBox
-
-var tracks_muted = [false,false,false,false,false,false,false]
-
-onready var inst_selec = get_parent().get_node("Instrument_Selection")
-
-func _on_Mute_pressed():
-	tracks_muted[inst_selec.num] = !tracks_muted[inst_selec.num]
-	get_tree().call_group("Key","set_muted")
--- GL Studio/Main/Play_Button.gd
+++
@@ -1,9 +0,0 @@
-extends Button
-
-onready var record_but = get_parent().get_node("Record_Button")
-
-func _on_Play_Button_toggled(button_pressed):
-	record_but.disabled = !record_but.disabled
-	Global.playing = !Global.playing
-	if button_pressed:
-		Global.start_music()
